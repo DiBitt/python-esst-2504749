a48a364ad23791bffaa2e488bd2f2bf7a7f4ebf4
DiBitt <dirk.bittger@blanco.de>
2025-11-20 09:06:50 +0000

    test

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,3 +1,8 @@
 # Das ist ein Kommentar in Ihrer Uebungsdatei
 
-print('Hallo Welt!')
+print('Hallo Dummi!')
+
+erste_variable = 3
+zweite_variable =2
+ergebnis_summe = erste_variable + zweite_variable
+print("ja", ergebnis_summe)
